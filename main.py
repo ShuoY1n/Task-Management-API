bf9bad27584813b894f2be5b8ad446184e94e403
ShuoY1n <jackshuoyi@gmail.com>
2025-01-03 17:54:16 -0500

    created basic fastapi infrastructure

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from datetime import date
 
 app = FastAPI()
+
+class Item(BaseModel):
+    id: id
+    title: str
+    description: str
+    priority: int
+    status: str
+    due_date: date
+    user_id: int
 
 @app.get("/")
 def index():
