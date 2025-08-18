c13309c0b3503134a0eb411d7b5de570bba48edd
ShuoY1n <jackshuoyi@gmail.com>
2025-08-18 17:17:21 -0600

    created some databases

--- database.py
+++ database.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "url"
+# DATABASE_URL = "postgresql://postgres:password@localhost:5432/item_db"
+# USER_DATABASE_URL = "postgresql://postgres:password@localhost:5432/user_db"
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "sqlite:///./items.db"
+USER_DATABASE_URL = "sqlite:///./users.db"
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL, echo=True)
+user_engine = create_engine(USER_DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+user_SessionLocal = sessionmaker(bind=user_engine)
 
 Base = declarative_base()
